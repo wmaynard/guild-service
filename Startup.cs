4e39a6af239c1cae3dcd66f07c8464b7da2e6159
Nathan Mac <nathan.mac@rumbleentertainment.com>
2023-01-13 10:27:58 -0800

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+using RCL.Logging;
+using Rumble.Platform.Common.Enums;
+using Rumble.Platform.Common.Web;
+
+namespace Rumble.Platform.GuildService;
+
+public class Startup : PlatformStartup
+{
+	protected override PlatformOptions ConfigureOptions(PlatformOptions options) => options
+	    .SetProjectOwner(Owner.Nathan)
+	    .SetTokenAudience(Audience.GuildService)
+	    .SetRegistrationName("Guild")
+#if DEBUG
+        .SetPerformanceThresholds(warnMS: 5_000, errorMS: 20_000, criticalMS: 300_000);
+#else
+        .SetPerformanceThresholds(warnMS: 500, errorMS: 2_000, criticalMS: 30_000);
+#endif
+}
